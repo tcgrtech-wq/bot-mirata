f9417143856f177e37a31cfb4ff951fee2bce940
tcgrtech-wq <tc.grtech@gmail.com>
2025-08-15 16:36:31 -0300

    Adicionando dependencias completas no replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,18 @@
 { pkgs }: {
   deps = [
     pkgs.glib
+    pkgs.nss
+    pkgs.fontconfig
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrender
+    pkgs.xorg.libXtst
+    pkgs.xorg.libXScrnSaver
+    pkgs.at-spi2-atk
+    pkgs.gtk3
   ];
 }
